9ef858f99aa77dbda904705f062617f80c898607
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 22:41:15 +0530

    added state to component Home.tsx

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface State {
+  id: string;
+  title: string;
+  text: string;
+  color: string;
+  date: string;
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const [notes, setNotes] = useState<State[]>([
+    {
+      id: new Date().toString(),
+      title: 'Meetings',
+      text: 'Schedule Meeting with UI/UX Team',
+      color: '#dfdfdf',
+      date: new Date().toString(),
+    },
+  ]);
+
+  return <div>{notes[0].id}</div>;
 };
 
 export default Home;
