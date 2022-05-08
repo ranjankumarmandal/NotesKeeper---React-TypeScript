67f90684e54700d0553121c1e52d66525bcb8be2
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 20:01:12 +0530

    added route for component About.tsx to /about in component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
       </Switch>
     </BrowserRouter>
   );
