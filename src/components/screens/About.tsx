e58191f99ea760a51208e8b732fa9ce99184c017
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 21:48:23 +0530

    added code for component About.tsx

--- src/components/screens/About.tsx
+++ src/components/screens/About.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='about'>
+      <h1>About This App</h1>
+      <h5>Version - 1.0.0</h5>
+      <h5>Description - This is an online notes keeper Application</h5>
+      <h5>
+        Made By -{' '}
+        <a
+          href='https://www.linkedin.com/in/ranjan-kumar-m-818567158/'
+          target='_blank'
+          style={{ textDecoration: 'none' }}
+        >
+          Ranjan Kumar Mandal
+        </a>
+      </h5>
+    </div>
+  );
 };
 
 export default About;
