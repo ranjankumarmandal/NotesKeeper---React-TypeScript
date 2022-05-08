a6c7da69bc05c704ca3acc8dd2a580d3d8162d63
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 21:21:58 +0530

    added component Container.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/screens/Home';
 import About from './components/screens/About';
+import Container from './components/utils/Container';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
+        <Container>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Container>
       </Switch>
     </BrowserRouter>
   );
