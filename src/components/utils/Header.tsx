4d9211a8061cc41db6f15ae0ec638e124c4e5967
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 22:56:45 +0530

    added Header code in component Header.tsx

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar fixed='top' bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>NotesKeeper</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
