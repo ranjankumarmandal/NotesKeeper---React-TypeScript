600fb10eec253508a6f2b41858de21e071a7ea3f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-08 19:55:22 +0530

    import component About.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/screens/Home';
+import About from './components/screens/About';
 import './App.css';
 
 function App() {
