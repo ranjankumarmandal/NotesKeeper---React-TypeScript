eee83fac46a27842527390c0c9a80fc6f2b272a6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-01 22:07:41 +0530

    added Header component to root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/screens/Home';
 import About from './components/screens/About';
 import Container from './components/utils/Container';
+import Header from './components/utils/Header';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Header />
         <Container>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
