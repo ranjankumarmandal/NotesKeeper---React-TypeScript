d3dbc3342f03acaec6b0b34e1831b9af1cca0ecf
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-02 22:20:48 +0530

    import Router component to root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/screens/Home';
 import About from './components/screens/About';
-import Container from './components/utils/Container';
-import Header from './components/utils/Header';
+import Router from './Router/Router';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
