87b0f50bbd86981cc17c2c1d3c39a50881e5fded
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-02 22:20:06 +0530

    completed component Router.tsx

---
+++ src/Router/Router.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../components/screens/Home';
+import About from '../components/screens/About';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
