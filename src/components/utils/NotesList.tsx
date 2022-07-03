6fecc98caf791561274d51a1f9749f5380f216b1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 15:25:36 +0530

    added that component returning function to jsx part of component NotesList.tsx

--- src/components/utils/NotesList.tsx
+++ src/components/utils/NotesList.tsx
@@ -3,13 +3,12 @@ import { NotesListProps } from '../../models/note.models';
 import Notes from './Notes';
 
 const NotesList = ({ notes }: NotesListProps) => {
-  const renderNotes = () => {
-    notes.map((note) => <Notes />);
-  };
+  const renderNotes = () => notes.map((note) => <Notes />);
 
   return (
     <>
       <h2 className='mt-3'>Notes</h2>
+      <div>{renderNotes()}</div>
     </>
   );
 };
