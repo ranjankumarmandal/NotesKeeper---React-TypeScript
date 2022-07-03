e4403f93db4b7a0b8e6308fb09dfb61151fc0aeb
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 16:28:43 +0530

    added all the note details inside jsx part of component Notes.tsx

--- src/components/utils/Notes.tsx
+++ src/components/utils/Notes.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NoteProps } from '../../models/note.models';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 const Notes = ({ note }: NoteProps) => {
   return (
     <div className='mb-3'>
       <Card>
         <Card.Title>{note.title}</Card.Title>
+        <Card.Text>{note.text}</Card.Text>
+        <Card.Subtitle className='text-muted'>{note.date}</Card.Subtitle>
+        <Button className='mb-3' variant='danger'>
+          Delete
+        </Button>
       </Card>
     </div>
   );
