06133cba877a1eec574de0072b6bf91dcea3a3a4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 11:48:47 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from './components/utils/Header';
-import Container from './components/utils/Container';
 import Router from './Router/Router';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
