edae072dd160374a41201ed77387f59436938625
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 15:18:22 +0530

    import component Notes insie componet NotesList.tsx

--- src/components/utils/NotesList.tsx
+++ src/components/utils/NotesList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NotesListProps } from '../../models/note.models';
+import Notes from './Notes';
 
 const NotesList = ({ notes }: NotesListProps) => {
   return (
