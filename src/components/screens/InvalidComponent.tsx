e270b3c3295781e13e5a5f09d2d15a3ef6dcb487
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 12:28:04 +0530

    completed component InvalidComponent.tsx

--- src/components/screens/InvalidComponent.tsx
+++ src/components/screens/InvalidComponent.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const InvalidComponent = () => {
-  return <div>InvalidComponent</div>;
+  return (
+    <>
+      <h1>404 : Page Not Found</h1>
+      <h3>
+        Facing technical issue, connect with -{' '}
+        <a
+          href='https://www.linkedin.com/in/ranjan-kumar-m-818367158/'
+          style={{ textDecoration: 'none' }}
+        >
+          Ranjan Kumar Mandal
+        </a>
+      </h3>
+    </>
+  );
 };
 
 export default InvalidComponent;
