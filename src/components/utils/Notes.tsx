54bbeff9ee2c7599ea9939764c7bf37e0ab9c765
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 16:36:54 +0530

    added onClick event handleDelete(id) to button in jsx part of component Notes.tsx

--- src/components/utils/Notes.tsx
+++ src/components/utils/Notes.tsx
@@ -10,7 +10,11 @@ const Notes = ({ note }: NoteProps) => {
           <Card.Title>{note.title}</Card.Title>
           <Card.Text>{note.text}</Card.Text>
           <Card.Subtitle className='text-muted'>{note.date}</Card.Subtitle>
-          <Button className='mt-3' variant='danger'>
+          <Button
+            className='mt-3'
+            variant='danger'
+            onClick={() => handleDelete(note.id)}
+          >
             Delete
           </Button>
         </Card.Body>
