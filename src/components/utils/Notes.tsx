17e1d79d112d92f1b894fef2e0bdc38c7a52a342
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 15:43:06 +0530

    added props type to component Note.tsx

--- src/components/utils/Notes.tsx
+++ src/components/utils/Notes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NoteProps } from '../../models/note.models';
 
-const Notes = (props: Note) => {
+const Notes = (props: NoteProps) => {
   return <div>Notes</div>;
 };
 
