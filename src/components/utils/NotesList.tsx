cdabcf61ddbf1b68b07a08d74435bf2d9240ba29
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 14:53:42 +0530

    added props type for component NotesLst.tsx

--- src/components/utils/NotesList.tsx
+++ src/components/utils/NotesList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NotesListProps } from '../../models/note.models';
 
-const NotesList = () => {
+const NotesList = (props: NotesListProps) => {
   return (
     <>
       <h2 className='mt-3'>Notes</h2>
