e3c695c84496d4a6cf8eb83469b4d2174ab8a66f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 13:42:10 +0530

    added more features in jsx part of component About.tsx

--- src/components/screens/About.tsx
+++ src/components/screens/About.tsx
@@ -4,7 +4,16 @@ const About = () => {
   return (
     <div>
       <h1>About This App</h1>
-      <h5>Version - 1.0.0</h5>
+      <h5>
+        Version -{' '}
+        <a
+          href='https://github.com/ranjankumarmandal/NotesKeeper---React-TypeScript'
+          target='_blank'
+          style={{ textDecoration: 'none' }}
+        >
+          1.0.0
+        </a>
+      </h5>
       <h5>Description - This is an online notes keeper Application</h5>
       <h5>
         Made By -{' '}
