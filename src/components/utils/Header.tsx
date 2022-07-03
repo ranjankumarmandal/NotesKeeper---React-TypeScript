729bc96fc5994aaae22b805ae189cc67ed1b22a9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 12:34:23 +0530

    resolved the bug: related to css styling|

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -4,20 +4,25 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Navbar fixed='top' bg='dark' variant='dark'>
-      <Container>
-        <Navbar.Brand>
-          <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>
-            NotesKeeper
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Brand>
-          <Link style={{ textDecoration: 'none', color: 'white' }} to='/about'>
-            About
-          </Link>
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+    <div style={{ marginBottom: '80px' }}>
+      <Navbar fixed='top' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand>
+            <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>
+              NotesKeeper
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Brand>
+            <Link
+              style={{ textDecoration: 'none', color: 'white' }}
+              to='/about'
+            >
+              About
+            </Link>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
