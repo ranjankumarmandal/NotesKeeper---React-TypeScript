16fe770296aeaf56a0d8549ac11db3d6decba4e8
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-03 17:28:51 +0530

    bug fix: setNotes issue

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Note } from '../../models/note.models';
 import { Container, Row, Col } from 'react-bootstrap';
 import NotesList from '../utils/NotesList';
 
 const Home = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: 'Meetings',
+      text: 'Schedule Meeting with UI/UX Team',
+      color: '#dfdfdf',
+      date: new Date().toString(),
+    },
+  ]);
+
   return (
     <>
       <Container>
         <Row>
           <Col>
-            <NotesList />
+            <NotesList notes={notes} setNotes={setNotes} />
           </Col>
         </Row>
       </Container>
--- src/models/note.models.ts
+++ src/models/note.models.ts
@@ -6,6 +6,11 @@ export interface Note {
   date: string;
 }
 
+export interface NotesListProps {
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+
 export interface NoteProps {
   note: Note;
   handleDelete: (id: string) => void;
